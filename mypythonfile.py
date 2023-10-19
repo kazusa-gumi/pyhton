a208f2ac679aee2fcd3261fbf8a0c4c141535c51
kazusa-gumi <jsva4127@yahoo.co.jp>
2023-10-19 23:53:23 +1100

    aaaa

--- mypythonfile.py
+++ mypythonfile.py
@@ -1,46 +1,55 @@
-# StudentRegistration.py
-# This program processes student details to register them into courses and then
-# displays this information onto the screen.
-# Author: Your Name Here
-# Date: Today's Date
-
 import tkinter as tk
 from tkinter import messagebox
 
-students = [] # Initialize students list
-totalFees = 0 # Initialize totalFees variable
+students = []
+totalFees = 0
 
 def printHeadings():
-    messagebox.showinfo("Headings", 'ID' + '\t' + 'NAME' + '\t' + 'COURSE' + '\t' + 'FEE')
+    heading = 'ID'.ljust(10) + 'NAME'.ljust(10) + 'COURSE'.ljust(10) + 'FEE'.ljust(10)
+    return heading
 
-def printStudentDetails(student): # Function to print student details
-    student_info = f"ID: {student['id']}\nName: {student['name']}\nCourse: {student['course']}\nFee: ${student['fee']}"
-    messagebox.showinfo("Student Details", student_info)
+def printStudentDetails(student):
+    student_info = f"{student['id']:<10}{student['name']:<10}{student['course']:<10}{student['fee']:<10}"
+    return student_info
 
 def inputStudentDetails():
     global totalFees
+    if len(students) >= 3:
+        messagebox.showerror("Error", "You can't register more than 3 students.")
+        return
     id = idEntry.get()
     name = nameEntry.get()
     course = courseEntry.get()
     fee = int(feeEntry.get())
 
-    totalFees += fee # Add the fee to the total fees
+    totalFees += fee
     students.append({'id': id, 'name': name, 'course': course, 'fee': fee})
+    idEntry.delete(0, tk.END)
+    nameEntry.delete(0, tk.END)
+    courseEntry.delete(0, tk.END)
+    feeEntry.delete(0, tk.END)
+
+    if len(students) == 3:
+        submitDetails()
 
 def outputStudentDetails():
-    if students:
-        for student in students:
-            printStudentDetails(student)
-            
-def outputTotalFee(): # Function to output total Fees
-    messagebox.showinfo("Total Fees", "\nTotal Fees for all students: $" + str(totalFees))
+    all_students = ''
+    for student in students:
+        all_students += printStudentDetails(student) + '\n'
+    return all_students
+
+def outputTotalFee():
+    return "\nTotal Fees for all students: $" + str(totalFees)
 
 def submitDetails():
-    inputStudentDetails()
-    outputStudentDetails()
-    outputTotalFee()
+    detail = printHeadings() + '\n' + outputStudentDetails() + outputTotalFee()
+    showDetailWindow(detail)
 
-# Create a Tkinter window
+def showDetailWindow(details):
+    detailWindow = tk.Toplevel(window)
+    detailWindow.geometry("500x500")
+    tk.Label(detailWindow, text=details, justify='left').pack()
+
 window = tk.Tk()
 
 tk.Label(window, text="ID").grid(row=0)
@@ -60,6 +69,6 @@ courseEntry.grid(row=2, column=1)
 feeEntry = tk.Entry(window)
 feeEntry.grid(row=3, column=1)
 
-tk.Button(window, text='Submit', command=submitDetails).grid(row=4, column=1)
+tk.Button(window, text='Submit', command=inputStudentDetails).grid(row=4, column=1)
 
 window.mainloop()
