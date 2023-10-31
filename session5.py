b9e14d2a868d889d224b5592768d65c5fc567c08
kazusa-gumi <jsva4127@yahoo.co.jp>
2023-10-31 11:48:01 +1100

    rangeメソッド追加

--- session5.py
+++ session5.py
@@ -1,8 +1,19 @@
 #cars = []
 #print(cars)
 
+# 0 1 2
+# -3 -2 -1 (negative index)
+# you reference from the end, and it starts -1
 cars = ['Saab', 'Volvo', 'BMW']
 print(cars)
 
-
 print(cars[2])
+print(cars[-3])
+
+fruits = ["apple", "banana", "peach", "oranges", "grapes"]
+print(fruits)
+
+
+# range(5) = [0,1,2,3,4]
+for i in range(len(fruits) ): # could use 5 instead
+    print(fruits[i])
