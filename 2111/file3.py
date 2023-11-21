f7f00215fb96fbe39847539d9cc8f78625ce7ffa
KAZUSA <knaoi@student.holmesglen.edu.au>
2023-11-21 11:41:00 +1100

    method追加

--- 2111/file3.py
+++ 2111/file3.py
@@ -3,3 +3,14 @@ f.write("\nLine Three")
 f.write("\nLine Four")
 f.write("\nLine Five")
 f.close()
+
+# read method
+f = open("text.txt", "r")
+for line in f:
+ print(line)
+f.close()       
+
+f = open("hello.txt", "r")
+for line in f:
+ print(line)
+f.close()     
