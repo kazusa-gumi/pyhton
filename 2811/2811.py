397d6ce3ddc86d17ba66a43d8669bcd263798e77
KAZUSA <knaoi@student.holmesglen.edu.au>
2023-11-28 11:22:14 +1100

    授業のclass追加

--- 2811/2811.py
+++ 2811/2811.py
@@ -1,6 +1,21 @@
+'''
 class Animal:
     def speak(self):
         print("Anumal speaks")
 
 my_animal = Animal()
 my_animal.speak()
+'''
+
+class student: 
+    schol_name="Holmesglen"
+    def __init__(self, name, roll_no):
+        self.name = name
+        self.roll_no = roll_no
+
+s1 = student("peter", 504)
+print(s1.name, s1.roll_no, student.schol_name)
+
+s2 = student("Poll", 555)
+print(s2.name, s2.roll_no, student.schol_name)
+
